ca7a91f3e2c6478e5e456bba44ba4e7610d4c58e
aniganesh <aniganesh741@gmail.com>
2022-08-07 16:52:04 +0530

    Add db auth

--- createDefaultUsers.js
+++ createDefaultUsers.js
@@ -6,7 +6,9 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-mongoose.connect("mongodb://localhost:27017/sftp");
+mongoose.connect(
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:27017/sftp?authSource=admin`
+);
 
 UserModel.deleteMany().then(() => {
   defaultUsers.forEach(async (user) => {
--- index.js
+++ index.js
@@ -13,9 +13,11 @@ const bodyParser = require("body-parser");
 const UserModel = require("./Models/user");
 const JWT = require("jsonwebtoken");
 const AUTH_TOKEN_KEY = "authToken";
-
-mongoose.connect("mongodb://localhost:27017/sftp");
 dotenv.config();
+
+mongoose.connect(
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:27017/sftp?authSource=admin`
+);
 
 const app = express();
 app.use(express.json());
